25f9128a4985c4aeb746b65af451156b6685b1e5
Itay Chamiel <itaych@gmail.com>
2019-04-23 01:51:13 +0300

    v1.0.7, 20190423 ---------------- * simplified mem_debug_check.

--- MEM_debug.h
+++ MEM_debug.h
@@ -14,7 +14,7 @@ freely, subject to the following restrictions:
     1. The origin of this software must not be misrepresented; you must not
     claim that you wrote the original software. If you use this software
     in a product or in the course of product development, an acknowledgment
-	in the product documentation would be appreciated but is not required.
+    in the product documentation would be appreciated but is not required.
 
     2. Altered source versions must be plainly marked as such, and must not be
     misrepresented as being the original software.
